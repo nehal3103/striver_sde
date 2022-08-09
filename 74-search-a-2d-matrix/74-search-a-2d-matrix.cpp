af11625b8c197c2dd19c9ec7a6c758d96ed0b94b
nehal3103 <79208096+nehal3103@users.noreply.github.com>
2022-08-09 21:31:58 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (51.76%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -3,21 +3,7 @@
 -linearly traverse the m*n matrix, search for target
 -if target present -> true
 -if not present -> false
-
-
-
-
-*/
-
-
-
-
-
-
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        bool ans = false;
+bool ans = false;
         int n = matrix.size();
         int m = matrix[0].size();
         for(int i = 0; i < n; i++){
@@ -28,6 +14,40 @@ public:
             }
         }
         return ans;
-        
+-tc -> o(m*n)
+-sc -> o(1)
+
+-next approach
+-go to all the rows, do binary search on each row
+-return true if found, false if not found
+-tc -> n * logm
+
+-optimal solution
+-the pointer should be at the last element of the first row
+-if found, return true
+-if target > elemet -> go downwards
+-if target < element, go towards the left
+
+*/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int i = 0;
+        int j = m-1;
+        while(i < n && j >= 0){
+            int element = matrix[i][j];
+            if(target == element){
+                return true;
+            }
+            if(target > element){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return false;      
     }
 };
