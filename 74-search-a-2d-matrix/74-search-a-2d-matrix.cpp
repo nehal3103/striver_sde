c7f1b5e3e1da13f3daf940957fb93e87fa5cd630
nehal3103 <79208096+nehal3103@users.noreply.github.com>
2022-08-09 21:47:29 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (51.76%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -20,19 +20,16 @@ bool ans = false;
 -next approach
 -go to all the rows, do binary search on each row
 -return true if found, false if not found
--tc -> n * logm
+-tc -> n * logm 
 
 -optimal solution
 -the pointer should be at the last element of the first row
 -if found, return true
 -if target > elemet -> go downwards
 -if target < element, go towards the left
-
-*/
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
+-tc->o(n);
+-sc->o(n);
+ int n = matrix.size();
         int m = matrix[0].size();
         int i = 0;
         int j = m-1;
@@ -49,5 +46,30 @@ public:
             }
         }
         return false;      
+    }
+*/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int low = 0; 
+        int high = m*n-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            int rowIndex = mid/m;
+            int colIndex = mid%m;
+            int element = matrix[rowIndex][colIndex];
+            if(target == element){
+                return true;
+            }
+            if(target > element){
+                low = mid + 1;
+            }
+            if(target < element){
+                high = mid - 1;
+            }
+        }   
+        return false;
     }
 };
