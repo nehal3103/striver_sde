d90349f90f8ac03367da8fdfcd438c159c94c64a
nehal3103 <79208096+nehal3103@users.noreply.github.com>
2022-08-12 17:05:32 +0530

    Time: 20 ms (64.11%), Space: 10.9 MB (47.70%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,7 +1,8 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
+/*
+-brute force
+-tc - o(n^2)
+-sc - o(n1)
+int n = nums.size();
         vector<int> ans;
         for(int i = 0; i < n-1; i++){
             for(int j = i+1; j < n; j++){
@@ -13,5 +14,19 @@ public:
             }
         }
         return ans;
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indices;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(indices.find(target-nums[i])!=indices.end()){
+                return{indices[target-nums[i]], i};
+            }
+            indices[nums[i]] = i;
+        }
+        return {};
     }
 };
