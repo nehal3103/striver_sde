be69c5c459d24b8f248be001ca3d1e0b63a95096
nehal3103 <79208096+nehal3103@users.noreply.github.com>
2022-08-19 14:37:29 +0530

    Time: 456 ms (24.48%), Space: 117.7 MB (65.52%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -12,11 +12,9 @@
 -brute force
 -take all the elements and store them in an array or vector
 -check if it is a palindrome or not
-*/
-class Solution {
-public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> v;
+-tc -> o(n)
+-sc -> o(n)
+vector<int> v;
         ListNode* temp = head;
         while(temp != NULL){
             v.push_back(temp->val);
@@ -30,6 +28,48 @@ public:
                 break;
             }                        
         }        
-        return ans;        
+        return ans;  
+        
+-better solution
+-find the middle of the ll
+-reverse the right half of the linked list
+-check if left and right are equal using two pointer approach
+-tc -> o(n)
+-sc -> o(1)
+*/
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode* pre = NULL;
+        ListNode* next = NULL;
+        while(head != NULL){
+            next = head->next;
+            head->next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return true;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != NULL && fast->next->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        slow->next = reverse(slow->next); 
+        slow = slow->next;
+        ListNode* dummy = head;
+        while(slow != NULL){
+            if(slow->val != dummy->val){
+                return false;
+            }
+            dummy = dummy->next;
+            slow = slow->next;
+        }
+        return true;
     }
 };
