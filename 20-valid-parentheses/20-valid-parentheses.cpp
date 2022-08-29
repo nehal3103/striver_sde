5821f08ce0294e2ebdaa02b10b58beebaf49cf35
nehal3103 <79208096+nehal3103@users.noreply.github.com>
2022-08-29 18:45:10 +0530

    Time: 4 ms (32.58%), Space: 6.3 MB (79.85%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                char c = st.top();
+                st.pop();
+                if(s[i] == ')' && c == '(' or s[i] == '}' && c == '{' or s[i] == ']' && c == '[') continue;
+                else return false;
+            }
+        }
+        return st.empty();       
+    }
+};
