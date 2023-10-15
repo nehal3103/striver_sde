c2bea371e87dd872f252e747052d2e96e8a8d7f3
Nehal Kohli <79208096+nehal3103@users.noreply.github.com>
2023-10-15 21:46:32 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -11,42 +11,40 @@ class Solution
         // Code here
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<pair<int,int>,int>>q;
-        int vis[n][m];
-        int delRow[] = {-1,0,1,0}; 
-        int delCol[] = {0,1,0,-1};
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2){
+        int visited[n][m];
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 2){ //rotten oranges
                     q.push({{i,j},0});
-                    vis[i][j] = 2;
+                    visited[i][j]=2;
                 }
                 else{
-                    vis[i][j] = 0;
+                    visited[i][j]=0;
                 }
             }
         }
         int tm = 0;
+        int drow[] = {-1, 0, +1, 0};
+        int dcol[] = {0, 1, 0, -1};
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
-            int t = q.front().second;
-            tm = max(tm,t);
+            int time = q.front().second;
+            tm = max(tm, time);
             q.pop();
-            for(int i = 0; i < 4; i++){
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol]==1 && vis[nrow][ncol]!=2){
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol] = 2;
+            for(int i=0; i<4; i++){
+                int nrow = row+drow[i];
+                int ncol = col+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && visited[nrow][ncol]!=2){
+                    q.push({{nrow,ncol}, time+1});
+                    visited[nrow][ncol]=2;
                 }
             }
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0;j  < m; j++){
-                if(vis[i][j] != 2 && grid[i][j] == 1){
-                    return -1;
-                }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(visited[i][j]!=2 && grid[i][j] == 1) return -1;
             }
         }
         return tm;
