b7f044c8a8d6fe6445b0f44811f4b865bd2d7d97
Nehal Kohli <79208096+nehal3103@users.noreply.github.com>
2023-10-15 19:37:51 +0530

    Added solution - LeetHub

--- Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -3,38 +3,29 @@
 using namespace std;
 
 // } Driver Code Ends
-
-
 class Solution {
 private:
-    void dfs(int row, int col, vector<vector<int>>&ans,vector<vector<int>>&image, int newColor, int delRow[], int delCol[], int iniColor){
+    void dfs(int row, int col, vector<vector<int>> &ans, vector<vector<int>>& image, int newColor, int iniColor, int drow[], int dcol[]){
         ans[row][col] = newColor;
         int n = image.size();
         int m = image[0].size();
-        for(int i = 0;i < 4; i++){
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor){
-                dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor);
-            }
+        for(int i=0; i<4; i++){
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==iniColor && ans[nrow][ncol]!=newColor){
+                dfs(nrow,ncol,ans,image,newColor,iniColor,drow,dcol);            }
         }
     }
-     
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        // Code here
+        // Code here 
         int iniColor = image[sr][sc];
         vector<vector<int>> ans = image;
-        int delRow[] = {-1,0,1,0};
-        int delCol[] = {0,1,0,-1};
-        dfs(sr,sc,ans,image,newColor,delRow,delCol,iniColor);
+        int drow[] = {-1, 0, +1, 0};
+        int dcol[] = {0, +1, 0, -1};
+        dfs(sr, sc, ans, image, newColor, iniColor, drow, dcol);
         return ans;
     }
-    
-    /*
-    tc - o(n*m)
-    sc - o(n*m)
-    */
 };
 
 //{ Driver Code Starts.
